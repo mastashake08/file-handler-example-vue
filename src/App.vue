2cc0727f2c880d3e1aa36bceda4379c99a5ccc1a
Hacker God <jyrone.parker@gmail.com>
2023-10-11 08:15:05 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,56 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Upload and examine files securely in the browser."></HelloWorld>
+    <button @click="getFile">Get File</button>
+    <div>
+      Name: {{ dataView.name }}
+    </div>
+    <hr/>
+    <div>
+      Size: {{ dataView.size }}
+    </div>
+    <hr/>
+    <!-- <div>
+      Text: {{ dataView.text }}
+    </div> -->
+    <hr/>
+    <div>
+      Last Modified: {{ dataView.lastModifiedDate }}
+    </div>
+    <hr/>
+    <div>
+      Type: {{ dataView.type }}
+    </div>
   </div>
 </template>
 
 <script>
+import { FileSystem } from 'shake-file'
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            file: {},
+            directory: {},
+            fileSystem: {},
+            dataView: {}
+        };
+    },
+    mounted() {
+        this.fileSystem = new FileSystem();
+    },
+    methods: {
+        async getFile() {
+            this.file = await this.fileSystem.getFile();
+            this.dataView = await this.fileSystem.getFileContents();
+        },
+        async getDir() {
+            this.directory = await this.fileSystem.getDir();
+        }
+    },
+    components: { HelloWorld }
 }
 </script>
 
