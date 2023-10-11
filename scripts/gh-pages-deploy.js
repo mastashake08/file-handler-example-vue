5e8b6b13de62c82d2c5b01498f0d17ed3e4d8852
Jyrone Parker <jyrone.parker@atlasied.com>
2023-10-11 12:15:40 -0400

    import

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
+import execa from "execa";
+import { existsSync } from "fs";
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
@@ -9,7 +9,7 @@ const fs = require("fs");
     await execa("npm", ["run", "build"]);
     // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const folderName = existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
